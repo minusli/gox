529932bc6abfbcc195e44a53d85311edf9d04535
lichengming <lichengming@bytedance.com>
2023-10-31 17:24:08 +0800

    数组下标越界

--- xslice/idx.go
+++ xslice/idx.go
@@ -1,16 +1,20 @@
 package xslice
 
-func First[T any](items []T) (ret T) {
+import (
+	"errors"
+)
+
+func First[T any](items []T) T {
 	if len(items) == 0 {
-		return
+		panic(errors.New("index out of bounds"))
 	}
 
 	return items[0]
 }
 
-func Last[T any](items []T) (ret T) {
+func Last[T any](items []T) T {
 	if len(items) == 0 {
-		return
+		panic(errors.New("index out of bounds"))
 	}
 
 	return items[len(items)-1]
@@ -18,7 +22,7 @@ func Last[T any](items []T) (ret T) {
 
 func Mid[T any](items []T) (left T, right T) {
 	if len(items) == 0 {
-		return
+		panic(errors.New("index out of bounds"))
 	}
 	if len(items)&1 == 1 {
 		return items[len(items)/2], items[len(items)/2]
