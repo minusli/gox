d3c6563b82c3f7547d27bf6a536326ef5c842198
lichengming <lichengming@bytedance.com>
2023-11-17 17:50:09 +0800

    FilterNil 中间判断指针类型，非指针数组直接返回原数组，同时增加 FilterBlank

--- xslice/filter.go
+++ xslice/filter.go
@@ -1,18 +1,24 @@
 package xslice
 
 import (
+	"reflect"
+
 	"github.com/minusli/gox/xtype"
 )
 
 func Filter[T any](items []T, filter func(T) bool) []T {
-	var retItems []T
+	if len(items) == 0 {
+		return items
+	}
+
+	var ret []T
 	for _, item := range items {
 		if filter(item) {
-			retItems = append(retItems, item)
+			ret = append(ret, item)
 		}
 	}
 
-	return retItems
+	return ret
 }
 
 func FilterZero[T xtype.Number](items []T) []T {
@@ -21,8 +27,22 @@ func FilterZero[T xtype.Number](items []T) []T {
 	})
 }
 
-func FilterNil[T any](items []*T) []*T {
-	return Filter(items, func(item *T) bool {
+func FilterNil[T any](items []T) []T {
+	if len(items) == 0 {
+		return items
+	}
+
+	if reflect.TypeOf(items[0]).Kind() != reflect.Pointer { // 非 ptr 直接返回
+		return items
+	}
+
+	return Filter(items, func(item T) bool {
 		return item != nil
+	})
+}
+
+func FilterBlank[T string | []byte](items []T) []T {
+	return Filter(items, func(s T) bool {
+		return len(s) == 0
 	})
 }
