2689a08a22d14f89917c306360496c29959401c5
lichengming <lichengming@bytedance.com>
2023-12-01 10:57:31 +0800

    新增 chunk 方法

--- xslice/other.go
+++ xslice/other.go
@@ -4,19 +4,30 @@ import (
 	"github.com/minusli/gox/xtype"
 )
 
-func Merge[T any](arrays ...[]T) []T {
-	var ret []T
-	for _, array := range arrays {
-		ret = append(ret, array...)
-	}
-
-	return ret
-}
-
 func Sum[V xtype.Number](items []V) V {
 	var ret V
 	for _, item := range items {
 		ret += item
 	}
+	return ret
+}
+
+func Chunk[T any](items []T, size int) [][]T {
+	var ret [][]T
+
+	if size <= 0 {
+		size = 1
+	}
+
+	for idx, item := range items {
+		chunkIdx := idx / size
+
+		if chunkIdx >= len(ret) {
+			ret = append(ret, []T{})
+		}
+
+		ret[chunkIdx] = append(ret[chunkIdx], item)
+	}
+
 	return ret
 }
--- xslice/other_test.go
+++ xslice/other_test.go
@@ -7,38 +7,6 @@ import (
 	"github.com/minusli/gox/xtype"
 )
 
-func TestMerge(t *testing.T) {
-	type args[T any] struct {
-		arrays [][]T
-	}
-	type testCase[T any] struct {
-		name string
-		args args[T]
-		want []T
-	}
-	tests := []testCase[string]{
-		{
-			name: "ok",
-			args: args[string]{
-				arrays: [][]string{
-					[]string{"a", "b", "c"},
-					[]string{"d", "e", "f"},
-					nil,
-					[]string{"a", "e"},
-				},
-			},
-			want: []string{"a", "b", "c", "d", "e", "f", "a", "e"},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Merge(tt.args.arrays...); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Merge() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestSum(t *testing.T) {
 	type args[V xtype.Number] struct {
 		items []V
@@ -61,6 +29,67 @@ func TestSum(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Sum(tt.args.items); got != tt.want {
 				t.Errorf("Sum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestChunk(t *testing.T) {
+	type args[T any] struct {
+		items []T
+		size  int
+	}
+	type testCase[T any] struct {
+		name string
+		args args[T]
+		want [][]T
+	}
+	tests := []testCase[int]{
+		{
+			name: "ok",
+			args: args[int]{
+				items: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14},
+				size:  5,
+			},
+			want: [][]int{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14}},
+		},
+		{
+			name: "blank",
+			args: args[int]{
+				items: []int{},
+				size:  5,
+			},
+			want: nil,
+		},
+		{
+			name: "nil",
+			args: args[int]{
+				items: nil,
+				size:  5,
+			},
+			want: nil,
+		},
+		{
+			name: "size<=0",
+			args: args[int]{
+				items: []int{1, 2, 3, 4, 5},
+				size:  -1,
+			},
+			want: [][]int{{1}, {2}, {3}, {4}, {5}},
+		},
+		{
+			name: "size>=len",
+			args: args[int]{
+				items: []int{1, 2, 3, 4, 5},
+				size:  10,
+			},
+			want: [][]int{{1, 2, 3, 4, 5}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Chunk(tt.args.items, tt.args.size); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Chunk() = %v, want %v", got, tt.want)
 			}
 		})
 	}
