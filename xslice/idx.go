822dc64158a4d2eb27903aae41d99525a2e3c1a4
lichengming <lichengming@bytedance.com>
2023-12-25 16:15:26 +0800

    xslice 中 idx 方法增加提供 default 的方法

--- xslice/idx.go
+++ xslice/idx.go
@@ -12,6 +12,14 @@ func First[T any](items []T) T {
 	return items[0]
 }
 
+func FirstWithDefault[T any](items []T, default_ T) T {
+	if len(items) == 0 {
+		return default_
+	}
+
+	return First(items)
+}
+
 func Last[T any](items []T) T {
 	if len(items) == 0 {
 		panic(errors.New("index out of bounds"))
@@ -20,14 +28,30 @@ func Last[T any](items []T) T {
 	return items[len(items)-1]
 }
 
+func LastWithDefault[T any](items []T, default_ T) T {
+	if len(items) == 0 {
+		return default_
+	}
+
+	return Last(items)
+}
+
 func Mid[T any](items []T) (left T, right T) {
 	if len(items) == 0 {
 		panic(errors.New("index out of bounds"))
 	}
+
 	if len(items)&1 == 1 {
 		return items[len(items)/2], items[len(items)/2]
 	} else {
 		return items[len(items)/2-1], items[len(items)/2]
 	}
+}
 
+func MidWithDefault[T any](items []T, default_ T) (left T, right T) {
+	if len(items) == 0 {
+		return default_, default_
+	}
+
+	return Mid(items)
 }
