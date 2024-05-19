f2315d030a2c548371a335f287c0b56ea989c517
lichengming <lichengming@bytedance.com>
2024-05-19 15:12:50 +0800

    调整 xerr 名称

--- xerr/err.go
+++ xerr/err.go
@@ -1,18 +1,18 @@
 package xerr
 
-type XErr struct {
+type Error struct {
 	Code int64
 	Msg  string
 }
 
-func (e XErr) Error() string {
+func (e Error) Error() string {
 	return e.Msg
 }
 
 //goland:noinspection GoUnusedGlobalVariable
 var (
-	BadRequest          = XErr{Code: 400, Msg: "bad request"}
-	Forbidden           = XErr{Code: 403, Msg: "forbidden"}
-	NotFound            = XErr{Code: 404, Msg: "not found"}
-	InternalServerError = XErr{Code: 500, Msg: "internal server error"}
+	BadRequest          = Error{Code: 400, Msg: "bad request"}
+	Forbidden           = Error{Code: 403, Msg: "forbidden"}
+	NotFound            = Error{Code: 404, Msg: "not found"}
+	InternalServerError = Error{Code: 500, Msg: "internal server error"}
 )
