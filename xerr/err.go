97c7312aad2e6742dcf4a9cafba50fb49a307035
lichengming <lichengming@bytedance.com>
2024-05-19 15:11:37 +0800

    大小写问题

--- xerr/err.go
+++ xerr/err.go
@@ -1,18 +1,18 @@
 package xerr
 
-type err struct {
+type XErr struct {
 	Code int64
 	Msg  string
 }
 
-func (e err) Error() string {
+func (e XErr) Error() string {
 	return e.Msg
 }
 
 //goland:noinspection GoUnusedGlobalVariable
 var (
-	BadRequest          = err{Code: 400, Msg: "bad request"}
-	Forbidden           = err{Code: 403, Msg: "forbidden"}
-	NotFound            = err{Code: 404, Msg: "not found"}
-	InternalServerError = err{Code: 500, Msg: "internal server error"}
+	BadRequest          = XErr{Code: 400, Msg: "bad request"}
+	Forbidden           = XErr{Code: 403, Msg: "forbidden"}
+	NotFound            = XErr{Code: 404, Msg: "not found"}
+	InternalServerError = XErr{Code: 500, Msg: "internal server error"}
 )
